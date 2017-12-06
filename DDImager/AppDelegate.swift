74e2b494f19a604353524736e81cf0dd20d1f335
MCJack123 <jackbruienne@yahoo.com>
2017-12-06 16:12:21 -0500

    Initial Commit

---
+++ DDImager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DDImager
+//
+//  Created by Homework User on 12/6/17.
+//  Copyright © 2017 JackMacWindows. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DDImager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DDImager
+//
+//  Created by Homework User on 12/6/17.
+//  Copyright © 2017 JackMacWindows. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
