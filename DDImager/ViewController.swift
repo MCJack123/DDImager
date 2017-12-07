44c8c2828004a69ce0db04db16328281dd66b806
MCJack123 <jackbruienne@yahoo.com>
2017-12-07 18:22:19 -0500

    Added copyright notices

--- DDImager/ViewController.swift
+++ DDImager/ViewController.swift
@@ -2,9 +2,19 @@
 //  ViewController.swift
 //  DDImager
 //
-//  Created by Homework User on 12/6/17.
-//  Copyright © 2017 JackMacWindows. All rights reserved.
-//  dd if=/Users/homeworkuser/Downloads/OS\ X\ Base\ System.dmg of=/dev/null
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import Cocoa
 
--- README.md
+++ README.md
@@ -7,3 +7,17 @@ This program is a graphical frontend for the UNIX program `dd`. It is written in
 ![Image 2](https://github.com/MCJack123/DDImager/raw/master/image2.png)
 
 In the current build, the file picker is disabled.
+
+## License
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
